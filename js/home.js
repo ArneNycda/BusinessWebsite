857e15eb09fd63098b41171a85306de1bcda0def
arne <arne@mintlab.nl>
2017-10-03 15:51:54 +0200

    Make holiday grid with loop

--- js/home.js
+++ js/home.js
@@ -1,3 +1,51 @@
+const holidayInfo = [
+    {
+        location: 'paris',
+        name: 'Paris',
+        description: 'text1',
+        image: 'parisbg'
+    },
+    {
+        location: 'amsterdam',
+        name: 'Amsterdam',
+        description: 'text2',
+        image: 'amsterdambg'
+    },
+    {
+        location: 'london',
+        name: 'London',
+        description: 'text3',
+        image: 'londonbg'
+    },
+    {
+        location: 'paris',
+        name: 'Paris',
+        description: 'text1',
+        image: 'parisbg'
+    },
+    {
+        location: 'amsterdam',
+        name: 'Amsterdam',
+        description: 'text2',
+        image: 'amsterdambg'
+    },
+    {
+        location: 'london',
+        name: 'London',
+        description: 'text3',
+        image: 'londonbg'
+    }
+];
+
+let holidayGridItems = '';
+
+holidayInfo.forEach(holiday => {
+    holidayGridItems += `<section class="tripcard">
+        <img class="tripimage" src="images/${holiday.image}.jpg">
+        <div class="tripdescription">${holiday.name}</div>
+    </section>`;
+});
+
 $(document).ready(function() {
     $(function(){
       $("#includeFooter").load("footer.html");
@@ -5,5 +53,9 @@ $(document).ready(function() {
 
     $(function(){
       $("#includeHeader").load("header.html");
+    });
+
+    $(function() {
+        $('#grid').html(holidayGridItems);
     });
 });
