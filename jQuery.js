26401903d769088736d86bef61d3bdfb5fedbf19
robertschaap <robert.schaap@mac.com>
2017-10-03 12:58:35 +0200

    Added reponsive nav

--- jQuery.js
+++ jQuery.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-    
+$(document).ready(() => {
+    $("#navi").click(function() {
+      $(this).toggleClass("openburger");
+      console.log("hello");
+    });
+
+    $(window).on("resize", function() {
+      if ($(window).width() > 600) {
+          $("#navi").removeClass();
+      }
+    }); 
 });
