97522502605f177b59ca5ad936a01126b5604dd7
arne <arne@mintlab.nl>
2017-10-05 12:56:09 +0200

    Make main content dynamic

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-require(['util/holidayInfo', 'header'], function(holidayInfo, header) {
+require(['util/holidayInfo'], function(holidayInfo) {
 
     let holidayGridItems = '';
 
@@ -12,7 +12,7 @@ require(['util/holidayInfo', 'header'], function(holidayInfo, header) {
     let holidayList = '';
 
     holidayInfo.forEach(holiday => {
-        holidayList += `<li><a href="${holiday.location}.html">${holiday.name}</a></li>`;
+        holidayList += `<li><a href="#${holiday.location}" class="contentLink">${holiday.name}</a></li>`;
     });
 
     $(document).ready(function() {
@@ -21,13 +21,73 @@ require(['util/holidayInfo', 'header'], function(holidayInfo, header) {
         });
 
         $(function(){
+            const location = window.location.hash === '' ? 'home' : window.location.hash;
+            
+            getContent(location);
+        });
+
+        const getContent = function(hash) {
+            const location = hash.replace('#', '');
+
+            if(location === 'home') {
+                $("#includeMain").load('home.html', function() {
+                    $('#grid').html(holidayGridItems);
+                });
+            } else if (location === 'about') {
+                $("#includeMain").load('about.html');
+            } else {
+                $(function(){
+                    const holiday = holidayInfo.find(holiday => holiday.location === location);
+                    const content = `<section id="gallery">
+                            <h2>${holiday.name}</h2>
+                            <div class="mySlides">
+                                <img class="mainimage" src="images/${holiday.image}.jpg" alt="Picture1">
+                            </div>  
+                            
+                            <div class="column"><img src="images/${holiday.image}.jpg" class="demo"></div>
+                            <div class="column"><img src="images/${holiday.location}2.jpg" class="demo"></div>
+                            <div class="column"><img src="images/${holiday.location}3.jpg" class="demo"></div>
+                            <div class="column"><img src="images/${holiday.location}4.jpg" class="demo"></div>
+                        </section>
+
+                        <div id="trip">
+                            <section id="tripdescription2">
+                                <h2>Description</h2>
+                                ${holiday.descriptionLong}</section>
+                            <aside id="amenities">
+                                <h2>What we offer</h2>
+                                <ul>
+                                    <li>4/5-Star Accomodation</li>
+                                    <li>Free Wifi in all locations</li>
+                                    <li>Pets Allowed</li>
+                                    <li>Parking Facilities</li>
+                                    <li>Airport Connections</li>
+                                    <li>Guided Tours</li>
+                                    <li>Our Detailed City Guide</li>
+                                </ul>
+                            </aside>
+                            <section id="howtobook">
+                                <button id="booknow">Book Now!</button>
+                            </section>
+                        </div>`;
+
+                    $("#includeMain").html(content, function() {
+                        $('#grid').html(holidayGridItems);
+                    });
+                });
+            }
+
+        }
+
+        $("body").on('click', '.contentLink', function() {
+            getContent($(this).attr('href'));
+        });
+
+
+        $(function(){
             $("#includeHeader").load("header.html", function() {
                 $('.dropdown-content ul').html(holidayList);
             });
-        });
-
-        $(function() {
-            $('#grid').html(holidayGridItems);
         });
     });
     
--- js/util/holidayInfo.js
+++ js/util/holidayInfo.js
@@ -4,18 +4,27 @@ define(function() {
             location: 'paris',
             name: 'Paris',
             description: 'City of Love',
+            descriptionLong: `<p>Paris is the capital and most populous city of France. It is at once a film set, one of the world's most beautiful cities &#40;with its golden stone facades and famous monuments &#41;, and a vibrant densely packed collection of villages.</p>
+                        <p>But however well you think you know Paris, there’s always something new to discover. Districts change, old ones get restored, whole new areas like Seine Rive Gauche around the new Bibliothèque Nationale go up, or the long shabby Faubourg-St-Denis suddenly becomes the latest hipster-hangout. There’s always a new bar or restaurant to check out, a new star baker, the latest gallery. Behind its grand palaces and boulevards, you can discover intimate courtyards and gardens; behind historic façades are craft workshops and eminent research institutions.</p>
+                        <p>Behind the grandiose palaces and boulevards, you can discover intimate courtyards and gardens; behind historic façades are craft workshops and eminent research institutions. Perhaps the true clue to Paris’s appeal is that despite being one of the most visited cities in the world it is also intensely lived-in. With its flats, playgrounds, food markets and cafés, there is no empty heart in the centre; and you, too, can play at being a Parisian.</p>`,
             image: 'parisbg'
         },
         {
             location: 'amsterdam',
             name: 'Amsterdam',
             description: 'Tour the Canals',
+            descriptionLong: `<p>Paris is the capital and most populous city of France. It is at once a film set, one of the world's most beautiful cities &#40;with its golden stone facades and famous monuments &#41;, and a vibrant densely packed collection of villages.</p>
+                        <p>But however well you think you know Paris, there’s always something new to discover. Districts change, old ones get restored, whole new areas like Seine Rive Gauche around the new Bibliothèque Nationale go up, or the long shabby Faubourg-St-Denis suddenly becomes the latest hipster-hangout. There’s always a new bar or restaurant to check out, a new star baker, the latest gallery. Behind its grand palaces and boulevards, you can discover intimate courtyards and gardens; behind historic façades are craft workshops and eminent research institutions.</p>
+                        <p>Behind the grandiose palaces and boulevards, you can discover intimate courtyards and gardens; behind historic façades are craft workshops and eminent research institutions. Perhaps the true clue to Paris’s appeal is that despite being one of the most visited cities in the world it is also intensely lived-in. With its flats, playgrounds, food markets and cafés, there is no empty heart in the centre; and you, too, can play at being a Parisian.</p>`,
             image: 'amsterdambg'
         },
         {
             location: 'london',
             name: 'London',
             description: 'The Largest European City',
+            descriptionLong: `<p>Paris is the capital and most populous city of France. It is at once a film set, one of the world's most beautiful cities &#40;with its golden stone facades and famous monuments &#41;, and a vibrant densely packed collection of villages.</p>
+                        <p>But however well you think you know Paris, there’s always something new to discover. Districts change, old ones get restored, whole new areas like Seine Rive Gauche around the new Bibliothèque Nationale go up, or the long shabby Faubourg-St-Denis suddenly becomes the latest hipster-hangout. There’s always a new bar or restaurant to check out, a new star baker, the latest gallery. Behind its grand palaces and boulevards, you can discover intimate courtyards and gardens; behind historic façades are craft workshops and eminent research institutions.</p>
+                        <p>Behind the grandiose palaces and boulevards, you can discover intimate courtyards and gardens; behind historic façades are craft workshops and eminent research institutions. Perhaps the true clue to Paris’s appeal is that despite being one of the most visited cities in the world it is also intensely lived-in. With its flats, playgrounds, food markets and cafés, there is no empty heart in the centre; and you, too, can play at being a Parisian.</p>`,
             image: 'londonbg'
         }
     ]}
