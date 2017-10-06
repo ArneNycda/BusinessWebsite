41692c8c20dbd8e4d7a6ec7224fa76383b51d4bd
arne <arne@mintlab.nl>
2017-10-06 16:30:43 +0200

    Make first character of location capital with function

--- js/util/holidayInfo.js
+++ js/util/holidayInfo.js
@@ -2,7 +2,6 @@ define(function() {
     return [
         {
             location: 'paris',
-            name: 'Paris',
             description: 'City of Love',
             descriptionLong: ['Paris is the capital and most populous city of France. It is at once a film set, one of the world\'s most beautiful cities &#40;with its golden stone facades and famous monuments&#41;, and a vibrant densely packed collection of villages',
                         'But however well you think you know Paris, there’s always something new to discover. Districts change, old ones get restored, whole new areas like Seine Rive Gauche around the new Bibliothèque Nationale go up, or the long shabby Faubourg-St-Denis suddenly becomes the latest hipster-hangout. There’s always a new bar or restaurant to check out, a new star baker, the latest gallery. Behind its grand palaces and boulevards, you can discover intimate courtyards and gardens; behind historic façades are craft workshops and eminent research institutions.',
@@ -21,7 +20,6 @@ define(function() {
         },
         {
             location: 'amsterdam',
-            name: 'Amsterdam',
             description: 'Tour the Canals',
             descriptionLong: ['Amsterdam is the capital and most populous municipalicy of the Kingdom of the Netherlands. The city is located in the province of North Holland in the west of the country. The metropolitan area comprises much of the northern part of the Randstad, one of the larger conurbations in Europe, with a population of approximately 7 million.',
                         'With world-class museums, quirky festivals, theatre, live music, laid-back bars and delightful restaurants, there\'s never a shortage of things to do in Amsterdam!',
@@ -40,7 +38,6 @@ define(function() {
         },
         {
             location: 'london',
-            name: 'London',
             description: 'The Largest European City',
             descriptionLong: ['There can be few more cosmopolitan cities on earth in the early 21 century and people pour in from across the world to visit, work or live. Colossal infrastructure projects such as Crossrail, King’s Cross-St Pancras and the Embassy Quarter and Battersea Power Station projects south of the river, are transforming the skyline.',
                         'Culturally, London is surfing the wave of recent events, including the Royal Wedding, Diamond Jubilee and London 2012 Olympics. The Queen Elizabeth Olympic Park has reopened and is anchoring another new quarter of London. Spaces such as the British Museum, the Royal National Theatre and both Tate galleries are expanding. The range of restaurants, bars, theatres, sports, pop-ups, performance art and large-scale events is astounding. Right here, right now, London is somewhere you have to be.',
@@ -59,7 +56,6 @@ define(function() {
         },
         {
             location: 'madrid',
-            name: 'Madrid',
             description: 'Sunny City',
             descriptionLong: ['Madrid is the capital of Spain and the largest municipality in both the Community of Madrid and Spain as a whole. The city has almost 3.166 million inhabitants with a metropolitan area population of approximately 6.5 million. It is the third-largest city in the European Union (EU) after London and Berlin, and its metropolitan area is the third-largest in the EU after those of London and Paris. The municipality itself covers an area of 604.3 km2',
                 'Madrid has many artistic attractions, thanks to the different styles that have left their imprint in the city over the centuries.',
@@ -78,7 +74,6 @@ define(function() {
         },
         {
             location: 'rome',
-            name: 'Rome',
             description: 'Capital City of an Old Empire',
             descriptionLong: ['Rome is the capital of Italy and a special comune (named Comune di Roma Capitale). Rome also serves as the capital of the Lazio region. With 2,877,215 residents in 1,285 km2 (496.1 sq mi), it is also the country\'s most populated comune. It is the fourth-most populous city in the European Union by population within city limits. It is the center of the Metropolitan City of Rome, which has a population of 4.3 million residents. Rome is located in the central-western portion of the Italian Peninsula, within Lazio (Latium), along the shores of the Tiber. The Vatican City is an independent country inside the city boundaries of Rome, the only existing example of a country within a city: for this reason Rome has been often defined as capital of two states.',
                 'What is there to really that you can’t say about Rome? Considered the center of the world for centuries, the birthplace of Caesar, and home to the Catholic Church, Rome is a city built on history. Rome is Rome. There’s no place like it on earth. The history, the crowds, the congestion, the traffic, and the ton of tourists. You walk down the street and next to a modern building are ruins dating back thousands of years. Roman cuisine is some of the best in Italy. Rome is tremendous and, though crowded, it is a city filled with life, beauty, and charm.',
@@ -97,7 +92,6 @@ define(function() {
         },
         {
             location: 'berlin',
-            name: 'Berlin',
             description: 'Historical Center of Europe',
             descriptionLong: ['Berlin is the capital and the largest city of Germany as well as one of its 16 constituent states. With a population of approximately 3.7 million, Berlin is the second most populous city proper in the European Union and the seventh most populous urban area in the European Union. Located in northeastern Germany on the banks of the rivers Spree and Havel, it is the centre of the Berlin-Brandenburg Metropolitan Region, which has roughly 6 million residents from more than 180 nations. Due to its location in the European Plain, Berlin is influenced by a temperate seasonal climate. Around one-third of the city\'s area is composed of forests, parks, gardens, rivers, canals and lakes.',
                 'For all its reputation as the capital of cool, Berlin makes a fabulous family destination. Berliners are generally tolerant of children, public transport is easy, cheap and fast, and the city brims with parks, playgrounds and lakes. Queues for the main attractions are usually shorter than in many other cities thanks to Berlin’s relatively small population (3.6 million) and impressive wealth of cultural delights, and it’s usually possible to beat the queues for big sights like the Reichstag and the TV Tower by simply booking online.'
