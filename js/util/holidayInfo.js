ce06fbea940a540a7c312fb666df92f32ac14883
arne <arne@mintlab.nl>
2017-10-06 14:46:24 +0200

    Add extra holidays

--- js/util/holidayInfo.js
+++ js/util/holidayInfo.js
@@ -56,6 +56,62 @@ define(function() {
                 'Guided Tours',
                 'Our Detailed City Guide'
             ]
+        },
+        {
+            location: 'madrid',
+            name: 'Madrid',
+            description: 'Sunny city',
+            descriptionLong: ['Madrid is the capital of Spain and the largest municipality in both the Community of Madrid and Spain as a whole. The city has almost 3.166 million inhabitants with a metropolitan area population of approximately 6.5 million. It is the third-largest city in the European Union (EU) after London and Berlin, and its metropolitan area is the third-largest in the EU after those of London and Paris. The municipality itself covers an area of 604.3 km2',
+                'Madrid has many artistic attractions, thanks to the different styles that have left their imprint in the city over the centuries.',
+                'From the remains of the old Arab wall, to small Gothic churches and early Renaissance works, Madrid boasts a multitude of rich and varied works of art. Madrid comes from the Arabic word Magerit ("mother of waters"), which was the name that was given to the fortress built on the banks of the Manzanares River by the Umayyad of Cordoba, Muhammed (823-886). Although the city grew under the Arabs for two hundred years, the only remains still standing from that period are the wall and a few towers, which were turned into bell towers. The arrival of Los Austrias –as the Hapsburg dynasty was known– in the 16th century, and at the period of maximum splendour in the 17th century, brought about the monuments that today make up one of the most famous parts of the city –the area of "Los Austrias"–, along with the Plaza Mayor square, and a number of beautiful sites churches and convents. The austere Baroque façades contrast with the luxurious interiors of the palaces.'
+            ],
+            image: 'madridbg',
+            amentities: [
+                '4/5-Star Accomodation',
+                'Free Wifi in all locations',
+                'Pets Allowed',
+                'Parking Facilities',
+                'Airport Connections',
+                'Guided Tours',
+                'Our Detailed City Guide'
+            ]
+        },
+        {
+            location: 'rome',
+            name: 'Rome',
+            description: '',
+            descriptionLong: ['Rome is the capital of Italy and a special comune (named Comune di Roma Capitale). Rome also serves as the capital of the Lazio region. With 2,877,215 residents in 1,285 km2 (496.1 sq mi), it is also the country\'s most populated comune. It is the fourth-most populous city in the European Union by population within city limits. It is the center of the Metropolitan City of Rome, which has a population of 4.3 million residents. Rome is located in the central-western portion of the Italian Peninsula, within Lazio (Latium), along the shores of the Tiber. The Vatican City is an independent country inside the city boundaries of Rome, the only existing example of a country within a city: for this reason Rome has been often defined as capital of two states.',
+                'What is there to really that you can’t say about Rome? Considered the center of the world for centuries, the birthplace of Caesar, and home to the Catholic Church, Rome is a city built on history. Rome is Rome. There’s no place like it on earth. The history, the crowds, the congestion, the traffic, and the ton of tourists. You walk down the street and next to a modern building are ruins dating back thousands of years. Roman cuisine is some of the best in Italy. Rome is tremendous and, though crowded, it is a city filled with life, beauty, and charm.',
+                'Rome is a city that needs no introduction, but a detailed guide to its rustic restaurants, hidden hotels, and many cultural attractions certainly won\'t hurt those looking for more than the canned tourist trip. With a cornucopia of exquisite experiences to be had—from savoring cornetti on the balcony of Audrey Hepburn\'s favorite hotel to gazing at the Bernini sculptures at the ever-serene Villa Borghese, visits to hip neighborhoods like Pigneto to stops by historical ruins, and more—we\'re sharing our favorite ways to make the most out of a visit to one of the world\'s oldest cities.'
+            ],
+            image: 'romebg',
+            amentities: [
+                '4/5-Star Accomodation',
+                'Free Wifi in all locations',
+                'Pets Allowed',
+                'Parking Facilities',
+                'Airport Connections',
+                'Guided Tours',
+                'Our Detailed City Guide'
+            ]
+        },
+        {
+            location: 'berlin',
+            name: 'Berlin',
+            description: 'Sunny city',
+            descriptionLong: ['Berlin is the capital and the largest city of Germany as well as one of its 16 constituent states. With a population of approximately 3.7 million, Berlin is the second most populous city proper in the European Union and the seventh most populous urban area in the European Union. Located in northeastern Germany on the banks of the rivers Spree and Havel, it is the centre of the Berlin-Brandenburg Metropolitan Region, which has roughly 6 million residents from more than 180 nations. Due to its location in the European Plain, Berlin is influenced by a temperate seasonal climate. Around one-third of the city\'s area is composed of forests, parks, gardens, rivers, canals and lakes.',
+                'For all its reputation as the capital of cool, Berlin makes a fabulous family destination. Berliners are generally tolerant of children, public transport is easy, cheap and fast, and the city brims with parks, playgrounds and lakes. Queues for the main attractions are usually shorter than in many other cities thanks to Berlin’s relatively small population (3.6 million) and impressive wealth of cultural delights, and it’s usually possible to beat the queues for big sights like the Reichstag and the TV Tower by simply booking online.'
+            ],
+            image: 'berlinbg',
+            amentities: [
+                '4/5-Star Accomodation',
+                'Free Wifi in all locations',
+                'Pets Allowed',
+                'Parking Facilities',
+                'Airport Connections',
+                'Guided Tours',
+                'Our Detailed City Guide'
+            ]
         }
     ]}
 );
